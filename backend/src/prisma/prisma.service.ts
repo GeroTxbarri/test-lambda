01093c263ac5e936ab70bee69dc07423e1fd26bb
GeroTxbarri <gerochavarri10@gmail.com>
2026-03-31 14:41:10 -0300

    se logro levantar correctamente el backend
    se solucionar problemas respecto a la version de prisma, y se logro levantar todo correctamente, y se probaron las funcionalidades de los contactos

--- backend/src/prisma/prisma.service.ts
+++ backend/src/prisma/prisma.service.ts
@@ -1,9 +1,13 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
-import { PrismaClient } from '../../generated/prisma/client';
+import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
+
+
+
   async onModuleInit() {
+    // Esto despierta la conexión apenas arranca NestJS
     await this.$connect();
   }
 }
