4171710fafbaeedc1ae3e2de542eacab96d6c1aa
GeroTxbarri <gerochavarri10@gmail.com>
2026-03-31 22:58:08 -0300

    pequeño cambio para el deploy

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,6 +5,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({ origin: '*' }); // Habilita CORS para permitir llamadas desde el frontend de Nextjs
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
